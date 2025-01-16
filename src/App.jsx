94acb341aea4aa678746e5e78e96ba3666b05722
ppirapat <pirapat.thana@gmail.com>
2025-01-16 16:44:04 +0100

    Components: Header, SearchBar, SideBar and Pages: Event Board, Event Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
-// Import React
+// Import React & Dependencies
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-// Import all components
-import Navbar from "./components/NavBar";
-import Sidebar from "./components/SideBar";
-import SearchAndCreateNewEvent from "./components/SearchAndCreateNewEvent";
-import MainContent from "./components/MainContent";
+// Import Pages
+import EventBoard from './pages/EventBoard'
+import CreateEvent from './pages/CreateEvent'
+import EventInfo from './pages/EventInfo'
+import InvitationFlyers from './pages/InvitationFlyers'
+import Guestlist from './pages/Guestlist'
+import Vendors from './pages/Vendors'
+import Budgeting from './pages/Budgeting'
+import GeneralSettings from './pages/GeneralSettings'
+
+// Import Components
+import Header from './components/Header'
+import Sidebar from "./components/SideBar"
+import './styles/App.css'
 
 // Main App component
 const App = () => {
   return (
-    <div style={styles.app}>
-      <Navbar />
-
-      <div style={styles.body}>
-        <Sidebar />
-
-        <div style={styles.main}>
-          <SearchAndCreateNewEvent />
-          
-          <MainContent />
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="content-wrapper">
+          <Sidebar />
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<EventBoard />} />
+              <Route path="/create-event" element={<CreateEvent />} />
+              <Route path="/event-info" element={<EventInfo />} />
+              <Route path="/invitation-flyers" element={<InvitationFlyers />}></Route>
+              <Route path="/guestlist" element={<Guestlist />}></Route>
+              <Route path="/vendors" element={<Vendors />}></Route>
+              <Route path="/budgeting" element={<Budgeting />}></Route>
+              <Route path="/general-settings" element={<GeneralSettings />}></Route>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
-// Inline styles for the app
-const styles = {
-  app: { fontFamily: "Arial, sans-serif" }, // Use a clean font
-  body: { display: "flex" },               // Flexbox for layout
-  main: { flex: 1, padding: "20px" },      // Main section with padding
-};
-
-// Export App component
 export default App;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,55 @@
 import React from "react";
-import "./Sidebar.css"; // Import the CSS file
+import {Link} from 'react-router-dom';
+import "../styles/components/Sidebar.css";
+// Import Sidebar Icons
+import EventBoardIcon from '../assets/sidebar-icons/event-dashboard-icon.png'
+import EventInfoIcon from '../assets/sidebar-icons/event-info-icon.png'
+import InvitationFlyerIcon from '../assets/sidebar-icons/flyer-icon.png'
+import GuestlistIcon from '../assets/sidebar-icons/guest-icon.png'
+import VendorsIcon from '../assets/sidebar-icons/vendor-icon.png'
+import BudgetingIcon from '../assets/sidebar-icons/budgeting-icon.png'
+import GeneralSettingsIcon from '../assets/sidebar-icons/general-settings-icon.png'
 
+{/* var accepts property called isEventCreated */}
 const Sidebar = () => {
     return (
-
         <div className="sidebar">
-            <div className="event-board">Event Board</div>
-            <div className="event-information">Event Information</div>
-            <div className="digital-flyers">Digital Flyers</div>
-            <div className="guests">Guests</div>
-            <div className="vendors">Vendors</div>
-            <div className="budgeting">Budgeting</div>
-            <div className="general-settings">General Settings</div>
-        </div>
-  );
+            <Link to="/">
+                <img src={EventBoardIcon} alt="Event Board" />
+            </Link>
+            <Link to="/invitation-flyers">
+                <img src={InvitationFlyerIcon} alt="Invitation Flyer Icon" />
+            </Link>
+            <Link to="/guestlist" className="guest-icon">
+                <img src={GuestlistIcon} alt="Guestlist Icon" />
+            </Link>
+            <Link to="/vendors">
+                <img src={VendorsIcon} alt="Vendors Icon" />
+            </Link>
+            <Link to="/budgeting">
+                <img src={BudgetingIcon} alt="Budgeting Icon" />
+            </Link>
+            <Link to="/general-settings" className="general-settings">
+                <img src={GeneralSettingsIcon} alt="General Settings Icon" />
+            </Link>
+        </div> 
+     );
 };
 
 export default Sidebar;
+
+// {/* If EventCreated true, expression in () will be evaluated */}
+// {isEventCreated && (
+//     <></>
+// )}
+
+
+        // <div className="sidebar">
+        //     <div className="event-board">Event Board</div>
+        //     <div className="event-information">Event Information</div>
+        //     <div className="digital-flyers">Digital Flyers</div>
+        //     <div className="guests">Guests</div>
+        //     <div className="vendors">Vendors</div>
+        //     <div className="budgeting">Budgeting</div>
+        //     <div className="general-settings">General Settings</div>
+        // </div>
