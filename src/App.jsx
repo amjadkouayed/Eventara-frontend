b81b27bfbf97b8b764205543208653b9f742973e
Amjad Kouayed <amjadkouayed@gmail.com>
2025-01-19 17:35:29 +0100

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 // Import React & Dependencies
-import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import Pages
-import EventBoard from './pages/EventBoard'
-import CreateEvent from './pages/CreateEvent'
-import EventInfo from './pages/EventInfo'
-import InvitationFlyers from './pages/InvitationFlyers'
-import Guestlist from './pages/Guestlist'
-import Vendors from './pages/Vendors'
-import Budgeting from './pages/Budgeting'
-import GeneralSettings from './pages/GeneralSettings'
+import EventBoard from "./pages/EventBoard";
+import CreateEvent from "./pages/CreateEvent";
+import EventInfo from "./pages/EventInfo";
+import InvitationFlyers from "./pages/InvitationFlyers";
+import Guestlist from "./pages/Guestlist";
+import Vendors from "./pages/Vendors";
+import Budgeting from "./pages/Budgeting";
+import GeneralSettings from "./pages/GeneralSettings";
 
 // Import Components
-import Header from './components/Header'
-import Sidebar from "./components/SideBar"
-import './styles/App.css'
+import Header from "./components/Header";
+import Sidebar from "./components/SideBar";
+import "./styles/App.css";
 
 // Main App component
 const App = () => {
@@ -30,11 +29,17 @@ const App = () => {
               <Route path="/" element={<EventBoard />} />
               <Route path="/create-event" element={<CreateEvent />} />
               <Route path="/event-info" element={<EventInfo />} />
-              <Route path="/invitation-flyers" element={<InvitationFlyers />}></Route>
+              <Route
+                path="/invitation-flyers"
+                element={<InvitationFlyers />}
+              ></Route>
               <Route path="/guestlist" element={<Guestlist />}></Route>
               <Route path="/vendors" element={<Vendors />}></Route>
               <Route path="/budgeting" element={<Budgeting />}></Route>
-              <Route path="/general-settings" element={<GeneralSettings />}></Route>
+              <Route
+                path="/general-settings"
+                element={<GeneralSettings />}
+              ></Route>
             </Routes>
           </div>
         </div>
