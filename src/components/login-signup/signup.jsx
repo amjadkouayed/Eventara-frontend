0b071a4ba55853a54bd49d829b0b3525b5b9b2c8
Amjad Kouayed <amjadkouayed@gmail.com>
2025-01-20 17:17:58 +0100

    connect login and signup to backend

--- src/components/login-signup/signup.jsx
+++ src/components/login-signup/signup.jsx
@@ -1,14 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import "./auth.css";
+import { useState } from "react";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await fetch("http://localhost:4000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: name, email: email, password: password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        navigate("/login");
+      } else {
+        setError(data.message || "signup failed");
+      }
+    } catch (error) {
+      setError("Failed signing up. Please try again later.");
+    }
+  };
 
   return (
     <div className="auth-container">
       <div className="auth-box">
         <h1 className="auth-title">Sign up</h1>
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={handleSignUp}>
           <div className="input-group">
             <label htmlFor="name" className="input-label" id="name-label">
               Name
@@ -18,6 +44,7 @@ const SignUp = () => {
               id="name"
               className="auth-input"
               placeholder="Enter your name"
+              onChange={(e) => setName(e.target.value)}
               required
             />
           </div>
@@ -30,6 +57,7 @@ const SignUp = () => {
               id="email"
               className="auth-input"
               placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
@@ -42,6 +70,7 @@ const SignUp = () => {
               id="password"
               className="auth-input"
               placeholder="Create a password"
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
@@ -49,6 +78,7 @@ const SignUp = () => {
             Sign Up
           </button>
         </form>
+        {error && <p className="error-message">{error}</p>}
         <div className="auth-footer">
           <p>
             Already have an account?
